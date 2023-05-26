5e5d79e2819276ad0d8ac2023fa2ac9baa3c3f34
arasad1 <asaduzzamanaj07@gmail.com>
2023-05-26 08:58:09 +0600

    featured section added in home page

--- src/pages/Home/Home/Featured/Featured.jsx
+++ src/pages/Home/Home/Featured/Featured.jsx
@@ -5,9 +5,9 @@ import './Featured.css';
 
 const Featured = () => {
     return (
-        <div className="featured-item">
+        <div className="featured-item  text-gray-100 py-20 my-20">
             <SectionTitle heading={"Featured Item"} subHeading={"check it out"}></SectionTitle>
-            <div className="md:flex justify-center items-center py-8 px-16">
+            <div className="md:flex justify-center items-center py-10  px-32">
                 <div>
                     <img src={featuredImg} alt="featuredImg" />
                 </div>
@@ -15,7 +15,7 @@ const Featured = () => {
                     <p>Aug 20, 2020</p>
                     <p className="uppercase">Where can i get some</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus velit deleniti architecto assumenda ipsa ex suscipit unde cum cumque, in vel autem aspernatur, ab facilis inventore nam placeat quae libero at dolorem nemo maiores aliquid tenetur! Quasi tenetur recusandae architecto unde officiis saepe, commodi odit magni repudiandae perferendis illum similique.</p>
-                    <button className="btn btn-outline">Button</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
