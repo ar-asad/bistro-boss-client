bc139a07650c1c869f8799f9f940a5aaa4ec1378
arasad1 <asaduzzamanaj07@gmail.com>
2023-05-26 21:04:09 +0600

    menucover section added in our menu page

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
     const menuItem = <>
-        <li><a>Submenu 1</a></li>
-        <li><a>Submenu 2</a></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/menu'>Our Menu</Link></li>
     </>
 
     return (
