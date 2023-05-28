19172aecab5ea0b26c825d27ea2d54438c77110d
arasad1 <asaduzzamanaj07@gmail.com>
2023-05-28 09:36:20 +0600

    todays offer section added in our menu pages

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,14 +1,28 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../../Shared/Cover/Cover";
 import menuImg from '../../../assets/menu/banner3.jpg';
+import desserImg from '../../../assets/menu/dessert-bg.jpeg';
+import useMenu from "../../../hooks/UseMenu";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const offered = menu.filter(item => item.category === 'offered')
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
+            {/* main cover */}
             <Cover img={menuImg} title='our menu'></Cover>
+            {/* offered menu items */}
+            <SectionTitle subHeading="Don't Miss" heading="Today's offer"></SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
         </div>
     );
 };
